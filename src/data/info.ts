c96cda86bb82414bb2acbfd8fcc6d3f9eff75cfd
Coch <constantin.verine@gmail.com>
2023-12-04 14:38:13 +0100

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,72 @@
+export const info = {
+  baseUrl: "https://Cochta.github.io",
+  name: "Constantin Verine",
+  jobDescription: "Software & Games Developer",
+  about: ["I'm a software & game developer based in Geneva, Switzerland.",
+  "I enjoy creating algorythms and all the backend code for applications, games or anything else.",
+  "I speak 4 languages: French, Russian, English and German.",
+  "My primary strengths are in C# and C++ and i can also use C, Python, LUA, PHP, JavaScript, Java, HTML, CSS, Markdown, SQL, Mongo DB and more.",
+  " I love to discover and learn new languages and technologies.",
+],
+
+  experience: [
+    {
+      name: "Swiss Army",
+      location: "Switzerland",
+      startDate: "Jan 2020",
+      endDate: "Apr 2022",
+      description: [
+        "I completed my military duty and rose to the rank of lieutenant, I also obtained the Level 1 leadership certificate for officers.",
+        "Here are some of the skills that i learned:" ,
+        "✓ Training in leadership and methodological skills",
+        "✓ Strengthening social skills",
+        "✓ Practical experience in everyday military life",
+        "✓ Capturing and structuring complex problems under high temporal and physical pressure",
+        "✓ Developing and implementing useful, targeted solutions",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "SAE Institute, Games programming",
+      location: "Carouge, Geneva, Switzerland",
+      startDate: "2022",
+      endDate: "Present",
+      description: ["Bachelor of Science"],
+    },
+    {
+      name: "CFPT Informatique, Software development ",
+      location: "Lancy, Geneva, Switzerland",
+      startDate: "2016",
+      endDate: "2019",
+      description: ["Swiss Federal VET Diploma (CFC)"],
+    },
+  ],
+
+  socialMedia: {
+    //facebook: "",
+    //twitter: "",
+    github: "https://github.com/cochta",
+    email: "mailto:constantin.verine@gmail.com",
+    //linkedin: "",
+    itchio: "https://cochta.itch.io/",
+  },
+
+  projects: [
+    {
+      title: "Rambouille",
+      isFeatured: true,
+      thumbnail: "/assets/images/Rambouille.png",
+      githubUrl: "https://github.com/Cochta/Rambouille",
+      liveUrl: "https://cochta.itch.io/rambouille",
+    },
+    {
+      title: "Pandora's Box",
+      isFeatured: true,
+      thumbnail: "/assets/images/Box.jpg",
+      githubUrl: "https://github.com/Cochta/Pandora-s-box",
+      // liveUrl: "https://github.com/Cochta/",
+    },
+  ],
+};
